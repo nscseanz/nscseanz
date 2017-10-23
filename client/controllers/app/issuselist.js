d4d0b2628aeb26e0468f9fc7be3c11a528eb3456
Sean Zhang <Sean.Zhang@multek.com>
2017-10-23 01:09:11 -0700

    [Studio] Add screen "IssuseList"

---
+++ client/controllers/app/issuselist.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_issuselist', app_issuselist);
+function app_issuselist($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.issuselist', {
+        views: {
+            app: {
+                controller: 'app_issuselist',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.issuselist');
+                }
+            }
+        }
     });
 });
