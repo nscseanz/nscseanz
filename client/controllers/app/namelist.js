df3eda9370d6a1b1df60f844a7c134c340a3916e
Sean Zhang <Sean.Zhang@multek.com>
2017-10-24 21:14:59 -0700

    [Studio] Add screen "NameList"

---
+++ client/controllers/app/namelist.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_namelist', app_namelist);
+function app_namelist($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -60,5 +60,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.namelist', {
+        views: {
+            app: {
+                controller: 'app_namelist',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.namelist');
+                }
+            }
+        }
     });
 });
